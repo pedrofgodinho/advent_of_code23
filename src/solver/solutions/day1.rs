667c550004a5742f39c0d321666f68f49eba660e
Pedro Godinho <pedro.fernandez.godinho@gmail.com>
2023-12-01 07:08:43 +0000

    Add day 1 solution

--- src/solver/solutions/day1.rs
+++ src/solver/solutions/day1.rs
@@ -1,7 +1,63 @@
-pub fn part1(_input: &str) -> String {
-    "0".to_owned()
+pub fn part1(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let mut iter = line.chars().filter_map(|digit| digit.to_digit(10));
+            let first = iter.next().unwrap();
+            let last = iter.last().unwrap_or(first);
+            first * 10 + last
+        })
+        .sum::<u32>()
+        .to_string()
 }
 
-pub fn part2(_input: &str) -> String {
-    "0".to_owned()
+pub fn part2(input: &str) -> String {
+    let sum: u32 = input
+        .lines()
+        .map(|line| {
+            let mut first = 255;
+            let mut last = 255;
+            for i in 0..line.len() {
+                let digit = if let Some(digit) = line.chars().nth(i).unwrap().to_digit(10) {
+                    Some(digit as u8)
+                } else {
+                    get_digit(&line[i..])
+                };
+                if let Some(digit) = digit {
+                    if first == 255 {
+                        first = digit;
+                    }
+                    last = digit;
+                }
+            }
+            first as u32 * 10 + last as u32
+        })
+        .sum();
+    sum.to_string()
+}
+
+fn get_digit(string: &str) -> Option<u8> {
+    if string.starts_with("one") {
+        Some(1)
+    } else if string.starts_with("two") {
+        Some(2)
+    } else if string.starts_with("three") {
+        Some(3)
+    } else if string.starts_with("four") {
+        Some(4)
+    } else if string.starts_with("five") {
+        Some(5)
+    } else if string.starts_with("six") {
+        Some(6)
+    } else if string.starts_with("seven") {
+        Some(7)
+    } else if string.starts_with("eight") {
+        Some(8)
+    } else if string.starts_with("nine") {
+        Some(9)
+    } else if string.starts_with("zero") {
+        Some(0)
+    } else {
+        None
+    }
 }

e6b506fc234f843fb11ea6ad556ac7798d6d2aeb
Pedro Godinho <pedro.fernandez.godinho@gmail.com>
2023-12-02 23:26:50 +0000

    Add day 3 skeleton

---
+++ src/solutions/day3.rs
@@ -0,0 +1,21 @@
+use super::Solution;
+
+pub struct Day3 {}
+
+impl Day3 {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Solution for Day3 {
+    fn part1(&self, _input: &str) -> String {
+        "".to_owned()
+    }
+
+    fn part2(&self, _input: &str) -> String {
+        "".to_owned()
+    }
+
+    fn setup(&mut self) {}
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,12 +1,17 @@
 mod day1;
 mod day2;
+mod day3;
 
 pub trait Solution: Sync + Send {
-    fn setup(self: &mut Self);
-    fn part1(self: &Self, input: &str) -> String;
-    fn part2(self: &Self, input: &str) -> String;
+    fn setup(&mut self);
+    fn part1(&self, input: &str) -> String;
+    fn part2(&self, input: &str) -> String;
 }
 
-pub fn get_solutions() -> [Box<dyn Solution>; 2] {
-    [Box::new(day1::Day1), Box::new(day2::Day2 {})]
+pub fn get_solutions() -> [Box<dyn Solution>; 3] {
+    [
+        Box::new(day1::Day1),
+        Box::new(day2::Day2 {}),
+        Box::new(day3::Day3::new()),
+    ]
 }

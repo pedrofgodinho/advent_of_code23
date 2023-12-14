b03f27ea30f5d06c87101a107fa4feccf5fb4506
Pedro Godinho <pedro.fernandez.godinho@gmail.com>
2023-12-14 00:02:23 +0000

    Refactored runner

--- src/runner.rs
+++ src/runner.rs
@@ -6,6 +6,8 @@ use std::{
     time::{Duration, Instant},
 };
 
+use rayon::prelude::*;
+
 use crate::solutions::{get_parser_fns, ParserFn};
 
 #[derive(Debug)]
@@ -17,118 +19,71 @@ struct DayResult {
     part2_result: Option<String>,
 }
 
+#[derive(Debug)]
+struct CompleteDayResult {
+    day: usize,
+    io_duration: Duration,
+    day_result: DayResult,
+}
+
+struct Totals {
+    io: Duration,
+    parse: Duration,
+    part1: Duration,
+    part2: Duration,
+}
+
+impl Totals {
+    fn new() -> Self {
+        Self {
+            io: Duration::ZERO,
+            parse: Duration::ZERO,
+            part1: Duration::ZERO,
+            part2: Duration::ZERO,
+        }
+    }
+}
+
 pub fn run_days(days: &[usize], part: Option<i64>, input_dir: &Path) {
     let solutions = get_parser_fns();
-    let mut no_input = vec![];
-    let mut no_solution = vec![];
+
     let results = days
-        .iter()
+        .par_iter()
         .filter_map(|&day| {
             if solutions.len() >= day {
                 let start = Instant::now();
                 if let Ok(input) = get_input_for_day(input_dir, day) {
-                    let duration = Instant::now().duration_since(start);
-                    Some((day, duration, run_day(solutions[day - 1], part, input)))
+                    let io_duration = Instant::now().duration_since(start);
+                    Some(CompleteDayResult {
+                        day,
+                        io_duration,
+                        day_result: run_day(solutions[day - 1], part, input),
+                    })
                 } else {
-                    no_input.push(day);
+                    println!("Couldn't open input file for day {}", day);
                     None
                 }
             } else {
-                no_solution.push(day);
                 None
             }
         })
         .collect::<Vec<_>>();
 
-    let totals = results.iter().fold(
-        (
-            Duration::ZERO,
-            Duration::ZERO,
-            Duration::ZERO,
-            Duration::ZERO,
-        ),
-        |(io, parse, mut part1, mut part2), (_day, new_io, day_result)| {
-            if let Some(new_part1) = day_result.part1_duration {
-                part1 += new_part1;
+    let totals = results
+        .iter()
+        .fold(Totals::new(), |mut totals, complete_day_result| {
+            totals.io += complete_day_result.io_duration;
+            totals.parse += complete_day_result.day_result.parse;
+            if let Some(part1) = complete_day_result.day_result.part1_duration {
+                totals.part1 += part1;
             }
-            if let Some(new_part2) = day_result.part2_duration {
-                part2 += new_part2;
+            if let Some(part2) = complete_day_result.day_result.part2_duration {
+                totals.part2 += part2;
             }
-            (io + *new_io, parse + day_result.parse, part1, part2)
-        },
-    );
+            totals
+        });
 
-    let totals = if results.len() > 1 {
-        vec![[
-            "Total".to_owned(),
-            format!("{:?}", totals.0),
-            format!("{:?}", totals.1),
-            format!("{:?}", totals.2),
-            format!("{:?}", totals.3),
-            "-".to_owned(),
-            "-".to_owned(),
-        ]]
-    } else {
-        vec![]
-    };
-    let results = results
-        .into_iter()
-        .map(|(day, io_duration, day_result)| {
-            let day = day.to_string();
-            let io = format!("{:?}", io_duration);
-            let parse = format!("{:?}", day_result.parse);
-            let part1_duration = option_debug_to_string(day_result.part1_duration);
-            let part2_duration = option_debug_to_string(day_result.part2_duration);
-            let part1_result = option_display_to_string(day_result.part1_result);
-            let part2_result = option_display_to_string(day_result.part2_result);
-            [
-                day,
-                io,
-                parse,
-                part1_duration,
-                part2_duration,
-                part1_result,
-                part2_result,
-            ]
-        })
-        .chain(totals.into_iter())
-        .collect::<Vec<_>>();
-
-    let mut ascii_table = AsciiTable::default();
-    ascii_table.set_max_width(128);
-    ascii_table
-        .column(0)
-        .set_header("Day")
-        .set_align(Align::Right);
-    ascii_table
-        .column(1)
-        .set_header("File IO Duration")
-        .set_align(Align::Right);
-    ascii_table
-        .column(2)
-        .set_header("Parse Duration")
-        .set_align(Align::Right);
-    ascii_table
-        .column(3)
-        .set_header("Part 1 Duration")
-        .set_align(Align::Right);
-    ascii_table
-        .column(4)
-        .set_header("Part 2 Duration")
-        .set_align(Align::Right);
-    ascii_table
-        .column(5)
-        .set_header("Part 1 Result")
-        .set_align(Align::Right);
-    ascii_table
-        .column(6)
-        .set_header("Part 2 Result")
-        .set_align(Align::Right);
-    ascii_table.print(results);
-
-    for day in no_input {
-        println!("Couldn't open input file for day {}", day);
-    }
+    print_results(results, totals);
 }
 
 fn run_day(solution: ParserFn, part: Option<i64>, input: String) -> DayResult {
@@ -172,6 +127,93 @@ fn run_day(solution: ParserFn, part: Option<i64>, input: String) -> DayResult {
         part1_result,
         part2_result,
     }
+}
+
+fn print_results(results: Vec<CompleteDayResult>, totals: Totals) {
+    let totals = if results.len() > 1 {
+        vec![[
+            "Total".to_owned(),
+            format!("{:?}", totals.io),
+            format!("{:?}", totals.parse),
+            format!("{:?}", totals.part1),
+            format!("{:?}", totals.part2),
+            format!(
+                "{:?}",
+                totals.io + totals.parse + totals.part1 + totals.part2
+            ),
+            "-".to_owned(),
+            "-".to_owned(),
+        ]]
+    } else {
+        vec![]
+    };
+
+    let results = results
+        .into_iter()
+        .map(|complete_day_result| {
+            let day = complete_day_result.day.to_string();
+            let io = format!("{:?}", complete_day_result.io_duration);
+            let day_result = complete_day_result.day_result;
+            let parse = format!("{:?}", day_result.parse);
+            let part1_duration = option_debug_to_string(day_result.part1_duration);
+            let part2_duration = option_debug_to_string(day_result.part2_duration);
+            let total_duration = option_debug_to_string(Some(
+                complete_day_result.io_duration
+                    + day_result.parse
+                    + day_result.part1_duration.unwrap_or(Duration::ZERO)
+                    + day_result.part2_duration.unwrap_or(Duration::ZERO),
+            ));
+            let part1_result = option_display_to_string(day_result.part1_result);
+            let part2_result = option_display_to_string(day_result.part2_result);
+            [
+                day,
+                io,
+                parse,
+                part1_duration,
+                part2_duration,
+                total_duration,
+                part1_result,
+                part2_result,
+            ]
+        })
+        .chain(totals.into_iter())
+        .collect::<Vec<_>>();
+
+    let mut ascii_table = AsciiTable::default();
+    ascii_table.set_max_width(128);
+    ascii_table
+        .column(0)
+        .set_header("Day")
+        .set_align(Align::Right);
+    ascii_table
+        .column(1)
+        .set_header("File IO")
+        .set_align(Align::Right);
+    ascii_table
+        .column(2)
+        .set_header("Parse")
+        .set_align(Align::Right);
+    ascii_table
+        .column(3)
+        .set_header("Part 1")
+        .set_align(Align::Right);
+    ascii_table
+        .column(4)
+        .set_header("Part 2")
+        .set_align(Align::Right);
+    ascii_table
+        .column(5)
+        .set_header("Total")
+        .set_align(Align::Right);
+    ascii_table
+        .column(6)
+        .set_header("Part 1 Result")
+        .set_align(Align::Right);
+    ascii_table
+        .column(7)
+        .set_header("Part 2 Result")
+        .set_align(Align::Right);
+    ascii_table.print(results);
 }
 
 fn option_debug_to_string<T: fmt::Debug>(t: Option<T>) -> String {
